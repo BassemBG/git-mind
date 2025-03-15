410050de8ecac448d1b9f3e1af910a0517a76279
BassemBG <bgbassem8@gmail.com>
2025-03-15 01:04:54 +0100

    implemented refetch hook to invalidate all queries after project creation (this makes projects list in sidebar automatically show the newly-added proj

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import useRefetch from '@/hooks/use-refetch'
 import { api } from '@/trpc/react'
 import { FormInput, Loader2 } from 'lucide-react'
 import React from 'react'
@@ -16,7 +17,7 @@ type FormInput = {
 const CreatePage = () => {
     const {register, handleSubmit, reset} = useForm<FormInput>();
     const createProject = api.project.createProject.useMutation();
-
+    const refetch = useRefetch();
     function onSubmit(data: FormInput){
 
         createProject.mutate({
@@ -26,6 +27,7 @@ const CreatePage = () => {
         }, {
             onSuccess: () => {
                 toast.success('Project created successfully!')
+                refetch();
                 reset();
             },
             onError: () => {
